e69109045e4d66ec9fb65bbb55fc472fb089c867
Janne Suominen <janne.suominen@siili.com>
2022-12-21 14:20:52 +0200

    LOM-229: Add reverse proxy module + config. Remove 403 redirect from Tunnistamo.

---
+++ public/sites/default/staging.settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['helfi_proxy.settings']['asset_path'] = 'staging-lomake-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'staging-lomakkeet',
+  'fi' => 'staging-forms',
+  'sv' => 'staging-blanketter'
+];
---
+++ public/sites/default/testing.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$config['helfi_proxy.settings']['asset_path'] = 'test-lomake-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'test-lomakkeet',
+  'fi' => 'test-forms',
+  'sv' => 'test-blanketter'
+];
