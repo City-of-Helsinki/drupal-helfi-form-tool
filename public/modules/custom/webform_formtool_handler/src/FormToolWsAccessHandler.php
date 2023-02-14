d49efae27e1336ac9c2e839cbb20a4fadda7e07a
oskarimysteerio <43133397+oskarimysteerio@users.noreply.github.com>
2023-02-14 09:00:43 +0200

    LOM-387: Disable service return link module

--- public/modules/custom/webform_formtool_handler/src/FormToolWsAccessHandler.php
+++ public/modules/custom/webform_formtool_handler/src/FormToolWsAccessHandler.php
@@ -61,7 +61,7 @@ class FormToolWsAccessHandler extends WebformSubmissionAccessControlHandler {
       return WebformAccessResult::allowed();
     }
 
-    // If user does not have either helsinki profile role, they do not have access
+    // If user doesn't have either helsinki profile role, they don't have access
     // and if they do, they must be the submitter below.
     if (!self::inArrayWildcard($userRoles, 'helsinkiprofiili')) {
       return WebformAccessResult::forbidden();
