99bec11f5f09b095e2280824e1ed241a3dc66738
Oskari Nevalainen <oskari@mysteerio.fi>
2023-03-09 11:24:45 +0200

    LOM-423: FormToolSubmission listener for sending notification to a service

---
+++ public/modules/custom/webform_formtool_handler/src/EventSubscriber/WebformSubmissionSubscriber.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\webform_formtool_handler\EventSubscriber;
+
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\webform_formtool_handler\Event\WebformSubmissionEvent;
+use GuzzleHttp\ClientInterface;
+
+/**
+ * Subscribes to webform submission event and sends notification to service.
+ */
+class WebformSubmissionSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Constructs a WebformSubmissionSubscriber listener.
+   *
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   Http client. Guzzle.
+   * @param \Drupal\Core\Logger\LoggerChannelFactory $logger
+   *   Logger factory.
+   */
+  public function __construct(
+    private ClientInterface $http_client,
+    private LoggerChannelFactory $logger
+    ) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[WebformSubmissionEvent::SUBMISSION_EVENT][] = ['onSubmissionCreate'];
+    return $events;
+  }
+
+  /**
+   * Send POST request to service.
+   *
+   * @param \Drupal\webform_formtool_handler\Event\WebformSubmissionEvent $event
+   *   A WebformSubmissionEvent event.
+   */
+  public function onSubmissionCreate(WebformSubmissionEvent $event) {
+
+    try {
+      $formToolSubmissionId = $event->getAtvDocument()->getId();
+      $endpoint = getenv('FORM_SUBMISSION_NOTIFICATION_ENDPOINT');
+      $password = getenv('FORM_SUBMISSION_NOTIFICATION_ENDPOINT_PASSWORD');
+
+      if (!$endpoint || !$password || !$formToolSubmissionId) {
+        return;
+      }
+
+      $response = $this->http_client->request('POST', $endpoint, [
+        'json' => [
+          'formID' => $formToolSubmissionId,
+        ],
+        'headers' => [
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/json',
+          'Authorization' => 'Basic ' . $password,
+        ],
+      ]);
+
+      $statusCode = $response->getStatusCode();
+
+      $this->logger->get('webform_formtool_handler')->info('Sent notification for @submissionId (@statusCode)', [
+        '@submissionId' => $formToolSubmissionId,
+        '@statusCode' => $statusCode,
+      ]);
+    }
+    catch (\Exception $e) {
+      $this->logger->get('webform_formtool_handler')->error('Failed to send form submission notification event: ' . $e->getMessage());
+    }
+  }
+
+}
