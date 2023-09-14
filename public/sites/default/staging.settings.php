ab82323557719752f99175adec33c72014e92f5d
Janne Suominen <janne.suominen@siili.com>
2023-09-14 15:43:41 +0300

    LOM-527: Add email overrides for form email

--- public/sites/default/staging.settings.php
+++ public/sites/default/staging.settings.php
@@ -6,3 +6,5 @@ $config['helfi_proxy.settings']['prefixes'] = [
   'fi' => 'staging-lomakkeet',
   'sv' => 'staging-blanketter'
 ];
+
+$config['webform.webform.todistusjaljennospyynto_tilaus']['third_party_settings']['form_tool_webform_parameters']['form_tool_webform_parameters']['email_notify'] = 'janne.suominen@siili.com';
